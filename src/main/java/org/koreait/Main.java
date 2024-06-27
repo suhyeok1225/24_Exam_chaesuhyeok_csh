0dfc8ffdb73be626ed96da946b06b2dd669ac3ef
suhyeok1225 <chaesh1225@gmail.com>
2024-06-27 14:33:03 +0900

    number2

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int num1, num2, sum;
-        num1 = 0;
-        num2 = 1;
-        sum = 1;
-        for (int i = 0; i < n -1; i++) {
-            System.out.print(sum +" ");
-            sum = num1 + num2;
-            num1 = num2;
-            num2 = sum;
+        int a = sc.nextInt();
+        for (int i=a; i>0; i--) {
+            for (int j=a-i; j>0; j--) {
+                System.out.print(" ");
+            }
+            for (int j=i; j>0; j--) {
+                System.out.print(" ");
+                System.out.print(j);
+            }
+            System.out.println();
         }
-    sc.close();
+        sc.close();
     }
 }
